5128bd2f38d88b9c96e2883b967833b9a99c4a28
Ming Chang <ming900518@gmail.com>
2020-05-17 21:37:48 +0800

    Upload files

---
+++ com.orgname.packagename/var/mobile/IPAInstTemp/signer.sh
@@ -0,0 +1,42 @@
+if [[ $EUID -ne 0 ]]; then
+echo "This script must be run as root"
+exit 1
+fi
+echo "Usage: bash signer.sh ipaname.ipa"
+
+if [ ! -f /usr/bin/ldid ] || [ ! -f /usr/bin/zip ] || [ ! -f /usr/bin/unzip ]; then
+echo "***You need "'zip'", "'unzip,'" and "'Link Identity Editor (ldid)'" installed from Cydia to begin***"
+echo "Exiting, one or more of the required programs is missing"
+exit
+fi
+
+if [$1 == ""]; then
+echo "You didn't specify an argument (ipa file), try again."
+exit
+fi
+
+
+
+makeTheDir () {
+DIRECTORY=/var/mobile/Documents/signed/
+if [ ! -d "$DIRECTORY" ]; then
+mkdir /var/mobile/Documents/signed/
+fi
+}
+
+
+
+CURRENTDIR=/var/mobile/Documents/Cracked/
+IPA="$*"
+echo Attempting to unzip .ipa
+unzip "$IPA"
+cd Payload
+NAME=$(ls -1)
+EXECUTABLE=${NAME%.app}
+cd "$(ls -1)"
+ldid -S "$EXECUTABLE"
+cd ..
+cd ..
+zip -r "$EXECUTABLE".ipa Payload
+rm -r Payload
+echo "[*]Generated signed .ipa in $(pwd)/"$EXECUTABLE".ipa"
